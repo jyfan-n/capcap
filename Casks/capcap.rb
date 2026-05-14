bd343481df635797153a1064a14388caad75c73a
luoyanlin <cnskyrin@gmail.com>
2026-05-14 11:33:09 +0800

    chore(brew): bump cask to 1.1.1

--- Casks/capcap.rb
+++ Casks/capcap.rb
@@ -1,6 +1,6 @@
 cask "capcap" do
-  version "1.0.3"
-  sha256 "1ef8de30886f27de90a40437222b5b73397cf894790648136e824305aa34a155"
+  version "1.1.1"
+  sha256 "7434b70788736506f7f02f8b5bd63b0e7f55af323108faef35e0b0030a722e7a"
 
   url "https://github.com/realskyrin/capcap/releases/download/release-v#{version}/capcap-#{version}-macos.zip"
   name "capcap"
